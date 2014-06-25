92065882b5546b6b76403063ddb8a4092a981725
Mauro Agnoletti <mauro.agnoletti@gmail.com>
2014-06-24 21:52:44 -0300

    More integration tests added

--- C#/Gamify.Sdk.IntegrationTests/GameIntegrationTests.cs
+++ C#/Gamify.Sdk.IntegrationTests/GameIntegrationTests.cs
@@ -3,7 +3,6 @@ using Gamify.Sdk.Contracts.Requests;
 using Gamify.Sdk.Setup;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Threading;
 
 namespace Gamify.Sdk.IntegrationTests
 {
@@ -25,7 +24,7 @@ namespace Gamify.Sdk.IntegrationTests
         {
             var notification = default(GameNotification);
             var notificationObject = default(object);
-            var player1Name = "player1";
+            var player1Name = GetPlayerName();
             var player1GameHandler = this.ConnectPlayer(player1Name);
 
             player1GameHandler.Notification += (sender, e) =>
@@ -34,7 +33,7 @@ namespace Gamify.Sdk.IntegrationTests
                 notificationObject = this.serializer.Deserialize<object>(notification.SerializedNotificationObject);
             };
 
-            var player2Name = "player2";
+            var player2Name = GetPlayerName();
             var player2GameHandler = this.ConnectPlayer(player2Name);
 
             Assert.AreEqual(player1Name, player1GameHandler.UserName);
@@ -51,9 +50,9 @@ namespace Gamify.Sdk.IntegrationTests
         [TestMethod]
         public void IT_When_CreateGame_Then_Success()
         {
-            var player1Name = "player1";
+            var player1Name = GetPlayerName();
             var player1GameHandler = this.ConnectPlayer(player1Name);
-            var player2Name = "player2";
+            var player2Name = GetPlayerName();
             var player2GameHandler = this.ConnectPlayer(player2Name);
 
             var notification = default(GameNotification);
@@ -87,6 +86,144 @@ namespace Gamify.Sdk.IntegrationTests
             Assert.AreEqual(player1Name, gameInviteNotificationObject.Player1Name);
             Assert.AreEqual(string.Format("{0}-vs-{1}", player1Name, player2Name), gameInviteNotificationObject.SessionName);
             Assert.IsTrue(string.IsNullOrEmpty(gameInviteNotificationObject.AdditionalInformation));
+        }
+
+        [TestMethod]
+        public void IT_When_AcceptGame_Then_Success()
+        {
+            var player1Name = GetPlayerName();
+            var player1GameHandler = this.ConnectPlayer(player1Name);
+            var player2Name = GetPlayerName();
+            var player2GameHandler = this.ConnectPlayer(player2Name);
+
+            var notification = default(GameNotification);
+            var notificationObject = default(object);
+
+            player1GameHandler.Notification += (sender, e) =>
+            {
+                notification = this.serializer.Deserialize<GameNotification>(e.SerializedNotification);
+                notificationObject = this.serializer.Deserialize<object>(notification.SerializedNotificationObject);
+            };
+            player2GameHandler.Notification += (sender, e) =>
+            {
+                var gameInviteNotification = this.serializer.Deserialize<GameNotification>(e.SerializedNotification);
+
+                if (gameInviteNotification.Type != (int)GameNotificationType.GameInvite)
+                {
+                    return;
+                }
+
+                var gameInviteNotificationObject = this.serializer.Deserialize<GameInviteNotificationObject>(gameInviteNotification.SerializedNotificationObject);
+
+                var acceptGameRequestObject = new GameAcceptedRequestObject
+                {
+                    PlayerName = player2Name,
+                    SessionName = gameInviteNotificationObject.SessionName
+                };
+                var acceptGameRequest = new GameRequest(GameRequestType.GameAccepted)
+                {
+                    Sender = player2Name,
+                    SerializedRequestObject = this.serializer.Serialize(acceptGameRequestObject)
+                };
+
+                player2GameHandler.OnMessage(this.serializer.Serialize(acceptGameRequest));
+            };
+
+            var createGameRequestObject = new CreateGameRequestObject
+            {
+                PlayerName = player1Name,
+                InvitedPlayerName = player2Name
+            };
+            var createGameRequest = new GameRequest(GameRequestType.CreateGame)
+            {
+                Sender = player1Name,
+                SerializedRequestObject = this.serializer.Serialize(createGameRequestObject)
+            };
+
+            player1GameHandler.OnMessage(this.serializer.Serialize(createGameRequest));
+
+            Assert.AreEqual((int)GameNotificationType.GameCreated, notification.Type);
+            Assert.IsNotNull(notificationObject);
+            Assert.IsTrue(notificationObject is GameCreatedNotificationObject);
+
+            var gameCreatedNotificationObject = notificationObject as GameCreatedNotificationObject;
+
+            Assert.AreEqual(player1Name, gameCreatedNotificationObject.Player1Name);
+            Assert.AreEqual(player2Name, gameCreatedNotificationObject.Player2Name);
+            Assert.AreEqual(string.Format("{0}-vs-{1}", player1Name, player2Name), gameCreatedNotificationObject.SessionName);
+            Assert.IsTrue(string.IsNullOrEmpty(gameCreatedNotificationObject.AdditionalInformation));
+        }
+
+        [TestMethod]
+        public void IT_When_RejectGame_Then_Success()
+        {
+            var player1Name = GetPlayerName();
+            var player1GameHandler = this.ConnectPlayer(player1Name);
+            var player2Name = GetPlayerName();
+            var player2GameHandler = this.ConnectPlayer(player2Name);
+
+            var notification = default(GameNotification);
+            var notificationObject = default(object);
+
+            player1GameHandler.Notification += (sender, e) =>
+            {
+                notification = this.serializer.Deserialize<GameNotification>(e.SerializedNotification);
+                notificationObject = this.serializer.Deserialize<object>(notification.SerializedNotificationObject);
+            };
+            player2GameHandler.Notification += (sender, e) =>
+            {
+                var gameInviteNotification = this.serializer.Deserialize<GameNotification>(e.SerializedNotification);
+
+                if (gameInviteNotification.Type != (int)GameNotificationType.GameInvite)
+                {
+                    return;
+                }
+
+                var gameInviteNotificationObject = this.serializer.Deserialize<GameInviteNotificationObject>(gameInviteNotification.SerializedNotificationObject);
+
+                var rejectGameRequestObject = new GameRejectedRequestObject
+                {
+                    PlayerName = player2Name,
+                    SessionName = gameInviteNotificationObject.SessionName
+                };
+                var rejectGameRequest = new GameRequest(GameRequestType.GameRejected)
+                {
+                    Sender = player2Name,
+                    SerializedRequestObject = this.serializer.Serialize(rejectGameRequestObject)
+                };
+
+                player2GameHandler.OnMessage(this.serializer.Serialize(rejectGameRequest));
+            };
+
+            var createGameRequestObject = new CreateGameRequestObject
+            {
+                PlayerName = player1Name,
+                InvitedPlayerName = player2Name
+            };
+            var createGameRequest = new GameRequest(GameRequestType.CreateGame)
+            {
+                Sender = player1Name,
+                SerializedRequestObject = this.serializer.Serialize(createGameRequestObject)
+            };
+
+            player1GameHandler.OnMessage(this.serializer.Serialize(createGameRequest));
+
+            Assert.AreEqual((int)GameNotificationType.GameRejected, notification.Type);
+            Assert.IsNotNull(notificationObject);
+            Assert.IsTrue(notificationObject is GameRejectedNotificationObject);
+
+            var gameRejectedNotificationObject = notificationObject as GameRejectedNotificationObject;
+
+            Assert.AreEqual(player1Name, gameRejectedNotificationObject.Player1Name);
+            Assert.AreEqual(player2Name, gameRejectedNotificationObject.Player2Name);
+            Assert.AreEqual(string.Format("{0}-vs-{1}", player1Name, player2Name), gameRejectedNotificationObject.SessionName);
+        }
+
+        private static string GetPlayerName()
+        {
+            var playerName = string.Format("player{0}", Guid.NewGuid());
+
+            return playerName;
         }
 
         private TestGameHandler ConnectPlayer(string playerName)
