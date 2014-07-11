38c77e92cc9f875a4b84a76313f28337b8a7f054
Mauro Agnoletti <mauro.agnoletti@gmail.com>
2014-07-11 12:32:25 -0300

    NuGet package updated and all authentication info removed from framework

--- C#/Gamify.Sdk/Contracts/Requests/PlayerConnectRequestObject.cs
+++ C#/Gamify.Sdk/Contracts/Requests/PlayerConnectRequestObject.cs
@@ -1,17 +1,7 @@
 ﻿namespace Gamify.Sdk.Contracts.Requests
 {
-    public enum GameAuthenticationType : int
-    {
-        None = 0,
-        Facebook = 1
-    }
-
     public class PlayerConnectRequestObject : IRequestObject
     {
         public string PlayerName { get; set; }
-
-        public string AccessToken { get; set; }
-
-        public int AuthenticationType { get; set; }
     }
 }
